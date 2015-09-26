22c92502f703faa63ac16b3f6245efb86133f339
Bertha <bertha.chan@mail.utoronto.ca>
2015-09-26 18:16:16 -0400

    Defined all variables. Need equations to determine profit/loss

--- exercise1.py
+++ exercise1.py
@@ -12,6 +12,13 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
+money = 2000
+share_value = 900.00
+money_paid = money * share_value
+buy_commission = 0.03 * money_paid
 
-money = 1000.00
-print(money)
+share_sold_total = money * 942.75
+sold_commission = share_sold_total * 0.03
+money_left = share_sold_total - money_paid - sold_commission - buy_commission
+
+
