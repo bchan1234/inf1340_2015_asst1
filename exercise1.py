9944ec4d849c1b732fd157b71ab034b2e9e0718a
PranjalSwami <pranjalsswami@gmail.com>
2015-10-07 00:13:06 -0400

    completed code for exercise one
    still need to add documentation and comments

--- exercise1.py
+++ exercise1.py
@@ -16,9 +16,24 @@ money = 2000
 share_value = 900.00
 money_paid = money * share_value
 buy_commission = 0.03 * money_paid
+total_money_paid = money_paid + buy_commission
+
 
 share_sold_total = money * 942.75
 sold_commission = share_sold_total * 0.03
-money_left = share_sold_total - money_paid - sold_commission - buy_commission
+#money_left = share_sold_total - money_paid - sold_commission - buy_commission
+total_money_recieved = share_sold_total - sold_commission
+
+money_left = total_money_recieved - total_money_paid
+
+if money_left > 0:
+    print('Lakshmi made a profit of ' + money_left)
+elif money_left < 0:
+    # inverting negative value to positive value
+    loss = money_left * -1
+    print('Lakshmi made a loss of ' + str(loss))
+else:
+    print('Lakshmi neither made a profit nor a loss')
+
 
 
