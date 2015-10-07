9b31479e7a0ab46fba9d71cb724f8d31ce2fe066
Bertha <bertha.chan@mail.utoronto.ca>
2015-10-07 17:16:57 -0400

    Completed. Could possibly add error msg if inputting answers other than y/n?

--- exercise3.py
+++ exercise3.py
@@ -18,15 +18,45 @@ def diagnose_car():
     Interactively queries the user with yes/no questions to identify a
     possible issue with a car.
 
-    Inputs:
+    Inputs: Y/N
 
-    Expected Outputs:
+    Expected Outputs: troubleshooting message
 
-    Errors:
+    Errors: when inputting answers other than y/n
 
     """
 
-    print("The battery cables may be damaged. Replace cables and try again.")
+    answer = raw_input("Is the car silent when you turn the key? ")
+    if answer == "y":
+        answer = raw_input("Are the battery terminals corroded? ")
+        if answer == "y":
+            print("Clean terminals and try starting again.")
+        if answer == "n":
+            print("Replace cables and try again.")
+        else:
+            print("Please input y/n answer only.")
+    if answer == "n":
+        answer = raw_input("Does the car make a clicking noise? ")
+        if answer == "y":
+            print("Replace the battery.")
+        if answer == "n":
+            answer = raw_input("Does the car crank up but fail to start? ")
+            if answer == "y":
+                print("Check spark plug connections.")
+            if answer == "n":
+                answer = raw_input("Does the engine start and then die? ")
+                if answer == "y":
+                    answer = raw_input("Does your car have fuel injection? ")
+                    if answer == "y":
+                        print("Get it in for service.")
+                    if answer == "n":
+                        print("Check to ensure the choke is opening and closing.")
+                    else:
+                        print("Please input y/n answer only.")
+                if answer == "n":
+                    print("Engine is not getting enough fuel. Clean fuel pump.")
+
+
 
 
 diagnose_car()
