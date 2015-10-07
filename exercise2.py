547d99da036292397c40d7fe2368651e951bde88
PranjalSwami <pranjalsswami@gmail.com>
2015-10-07 00:17:08 -0400

    Completed exercise 2
    need to add comments and documentation

--- exercise2.py
+++ exercise2.py
@@ -34,6 +34,14 @@ def name_that_shape():
         print("Pentagon")
     elif sides == 6:
         print("Hexagon")
+    elif sides == 7:
+        print("Heptagon")
+    elif sides == 8:
+        print("Octagon")
+    elif sides == 9:
+        print("Nonagon")
+    elif sides == 10:
+        print("Decagon")
     else:
         print("Error")
 
